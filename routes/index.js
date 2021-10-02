c02b089b4d4cea0b666c5b28801d9125f7cf33a5
kevinelfert <elferkevin@gmail.com>
2021-10-01 20:19:01 -0500

    Fixing view engine error and adding pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { getPlayPage } = require('./play');
+const { getUsers } = require('./users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/play', getPlayPage);
+router.get('/users', getUsers);
+
 
 module.exports = router;
---
+++ routes/play.js
@@ -0,0 +1,3 @@
+module.exports.getPlayPage = (req, res) => {
+    res.render('play', { title: 'Play' })
+};
