3ba44509f49722c53496b9382bef561c84bfdd12
kevinelfert <elferkevin@gmail.com>
2021-11-04 13:34:58 -0500

    Restructuring branch off docker-compose

--- routes/leaderboard.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router;
-const leaderboardController = require('../controllers/leaderboard_record_controller');
-
-router.get('/', leaderboardController.get_scores);
-router.post('/', leaderboardController.add_scores);
--- routes/lobbies.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router;
-const lobbyController = require('../controllers/lobby_controller');
-
-router.get('/', lobbyController.get_lobbies)
-router.post('/', lobbyController.create_lobby)
